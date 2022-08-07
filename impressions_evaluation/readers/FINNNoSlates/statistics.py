af9516e0d185a90ae64e86cfce0ffaebfadde8b4
Fernando Benjamin PEREZ MAURERA <fperezmaurera@gmail.com>
2022-08-07 16:22:00 -0400

    Created statistics file for the FINN.no Slates dataset.

---
+++ impressions_evaluation/readers/FINNNoSlates/statistics.py
@@ -0,0 +1,226 @@
+import logging
+from typing import cast
+
+import numpy as np
+import pandas as pd
+import sparse
+from recsys_framework_extensions.data.splitter import remove_records_by_threshold, apply_custom_function
+
+import impressions_evaluation.readers.FINNNoSlates.reader as finn_reader
+from tqdm import tqdm
+
+
+logger = logging.getLogger(__name__)
+
+tqdm.pandas()
+
+
+def _set_unique_items(
+    *,
+    df: pd.DataFrame,
+) -> set[int]:
+    df_items = df["item_id"].explode(
+        ignore_index=True,
+    ).dropna(
+        inplace=False,
+        how="any",
+        axis="index",
+    )
+
+    df_items_impressions = df["impressions"].explode(
+        ignore_index=True,
+    ).dropna(
+        inplace=False,
+        how="any",
+        axis="index",
+    )
+
+    unique_items = set(
+        df_items
+    ).union(
+        df_items_impressions
+    )
+
+    unique_items.discard(finn_reader.ITEM_ID_FILL_VALUE)
+    unique_items.discard(finn_reader.ITEM_ID_NON_INTERACTED)
+
+    return unique_items
+
+
+def _set_unique_users(
+    *,
+    df: pd.DataFrame,
+) -> set[int]:
+    df_users = df["user_id"].dropna(
+        how="any",
+        inplace=False,
+        axis="index",
+    )
+
+    return set(df_users)
+
+
+def _create_mapper_id_to_indices(
+    *,
+    set_ids: set,
+) -> dict[int, int]:
+    return {
+        data_id: assigned_idx
+        for assigned_idx, data_id in enumerate(set_ids)
+    }
+
+
+def convert_dataframe_to_sparse(
+    *,
+    df: pd.DataFrame,
+    users_column: str,
+    items_column: str,
+    shape: tuple,
+    mapper_user_id_to_idx: dict[int, int],
+    mapper_item_id_to_idx: dict[int, int],
+) -> sparse.COO:
+    df = df[[users_column, items_column]]
+
+    if df[items_column].dtype == "object":
+        df = cast(pd.DataFrame, df).explode(
+            column=items_column,
+            ignore_index=True,
+        ).dropna(
+            how="any",
+            axis="index",
+            inplace=False,
+        )
+
+    rows = df[users_column].to_numpy(dtype=np.int32)
+    cols = df[items_column].to_numpy(dtype=np.int32)
+
+    mask_array = cols >= finn_reader.ITEM_ID_DELETED_AT_DATA_COLLECTION
+
+    rows = rows[mask_array]
+    cols = cols[mask_array]
+
+    rows = np.array([
+        mapper_user_id_to_idx[user_id]
+        for user_id in rows
+    ], dtype=np.int32)
+    cols = np.array([
+        mapper_item_id_to_idx[item_id]
+        for item_id in cols
+    ], dtype=np.int32)
+    data = np.ones_like(rows, dtype=np.int32)
+
+    urm = sparse.COO(
+        (data, (rows, cols)),
+        has_duplicates=True,
+        shape=shape,
+    )
+    print(urm, urm.data.sum(), urm.nnz)
+
+    return urm
+
+
+def remove_interactions_from_uim(
+    *,
+    urm: sparse.COO,
+    uim: sparse.COO,
+) -> sparse.COO:
+    uim_dok = sparse.DOK.from_coo(uim)
+
+    for data_idx in range(urm.data.size):
+        row_idx = urm.coords[0, data_idx]
+        col_idx = urm.coords[1, data_idx]
+
+        uim_dok[row_idx, col_idx] = 0
+
+    uim = uim_dok.to_coo()
+    print(uim, uim.data.sum(), uim.nnz)
+
+    return uim
+
+
+def finn_no_slates_statistics_full_dataset() -> dict:
+    config = finn_reader.FinnNoSlatesConfig()
+
+    pandas_raw_data = finn_reader.PandasFinnNoSlateRawData(
+        config=config,
+    )
+
+    df_raw_data = pandas_raw_data.data
+
+    # We need to remove those interaction points with 0. As these are records with an "undefined" impression origin.
+    # These records all look like this: (user_id, item_id=0, impressions=[0, 0, ... 0]) so they do not bring any
+    # information and were mostly processing errors.
+    df_raw_data, _ = remove_records_by_threshold(
+        df=df_raw_data,
+        column="item_id",
+        threshold=[finn_reader.ITEM_ID_FILL_VALUE],
+        how="not_isin",
+    )
+    # We need to remove from the impressions the 'fill items' (AKA item_id = 0) and 'non-clicked items'
+    # (AKA item_id = 1)
+    df_raw_data, _ = apply_custom_function(
+        df=df_raw_data,
+        column="impressions",
+        axis="columns",
+        func=finn_reader.remove_non_clicks_on_impressions,
+        func_name=finn_reader.remove_non_clicks_on_impressions.__name__,
+        min_item_id=finn_reader.ITEM_ID_DELETED_AT_DATA_COLLECTION,
+    )
+
+    logger.debug("Computing set unique users")
+    unique_users = _set_unique_users(
+        df=df_raw_data,
+    )
+
+    logger.debug("Computing set unique items")
+    unique_items = _set_unique_items(
+        df=df_raw_data,
+    )
+
+    logger.debug("Computing mappers to users and items")
+    mapper_user_id_to_index = _create_mapper_id_to_indices(set_ids=unique_users)
+    mapper_item_id_to_index = _create_mapper_id_to_indices(set_ids=unique_items)
+
+    # We need to remove the non-click item from the matrices.
+    num_users, num_items = len(unique_users), len(unique_items)
+    common_sparse_shape = (num_users, num_items)
+    logger.debug(f"{num_users=} - {num_items=} - {common_sparse_shape=}")
+
+    logger.debug("Computing URM")
+    urm = convert_dataframe_to_sparse(
+        df=df_raw_data,
+        users_column="user_id",
+        items_column="item_id",
+        shape=common_sparse_shape,
+        mapper_user_id_to_idx=mapper_user_id_to_index,
+        mapper_item_id_to_idx=mapper_item_id_to_index,
+    )
+
+    logger.debug("Computing UIM from Interactions")
+    uim_with_interactions = convert_dataframe_to_sparse(
+        df=df_raw_data,
+        users_column="user_id",
+        items_column="impressions",
+        shape=common_sparse_shape,
+        mapper_user_id_to_idx=mapper_user_id_to_index,
+        mapper_item_id_to_idx=mapper_item_id_to_index,
+    )
+
+    logger.debug("Computing UIM without Interactions")
+    uim = remove_interactions_from_uim(
+        urm=urm,
+        uim=uim_with_interactions,
+    )
+
+    logger.debug("Creating statistics dictionary")
+    statistics = {
+        "dataset": "FINN.no Slates",
+        "num_users": num_users,
+        "num_items": num_items,
+        "num_interactions": urm.data.sum(),
+        "num_unique_interactions": urm.nnz,
+        "num_impressions": uim.data.sum(),
+        "num_unique_impressions": uim.nnz,
+    }
+
+    return statistics
--- impressions_evaluation/readers/MIND/statistics.py
+++ impressions_evaluation/readers/MIND/statistics.py
@@ -3,7 +3,8 @@ This module reads, processes, splits, creates impressiosn features, and saves in
 dataset.
 
 """
-from typing import cast
+import logging
+from typing import cast, Union
 
 import numpy as np
 import pandas as pd
@@ -12,12 +13,7 @@ import impressions_evaluation.readers.MIND.reader as mind_reader
 from tqdm import tqdm
 
 
-import logging
 logger = logging.getLogger(__name__)
-logger.debug(f"new-{__name__=} - {__file__=}")
-
-# logger_old = getLogger(__name__)
-# logger_old.debug(f"old-{__name__=} - {__file__=}")
 
 tqdm.pandas()
 
@@ -27,7 +23,7 @@ def _set_unique_items(
     df: pd.DataFrame,
     df_previous_interactions: pd.DataFrame,
 ) -> set[int]:
-    df_items_previous = df_previous_interactions["item_ids"].explode(
+    df_items_previous_interactions = df_previous_interactions["item_ids"].explode(
         ignore_index=True,
     ).dropna(
         inplace=False,
@@ -43,14 +39,20 @@ def _set_unique_items(
         axis="index",
     )
 
+    df_items_impressions = df["impressions"].explode(
+        ignore_index=True,
+    ).dropna(
+        inplace=False,
+        how="any",
+        axis="index",
+    )
+
     unique_items = set(
-        df_series_interactions
+        df_items_previous_interactions
     ).union(
-        df_series_interactions_impressions
+        df_items
     ).union(
-        df_series_impressions
-    ).union(
-        df_series_impressions_non_direct_link
+        df_items_impressions
     )
 
     return unique_items
@@ -74,12 +76,23 @@ def _set_unique_users(
     return set(df_users).union(df_previous_interactions)
 
 
+def _create_mapper_id_to_indices(
+    set_ids: set,
+) -> dict[str, int]:
+    return {
+        data_id: assigned_idx
+        for assigned_idx, data_id in enumerate(set_ids)
+    }
+
+
 def convert_dataframe_to_sparse(
     *,
     df: pd.DataFrame,
     users_column: str,
     items_column: str,
     shape: tuple,
+    mapper_user_id_to_idx: dict[str, int],
+    mapper_item_id_to_idx: dict[str, int],
 ) -> sparse.COO:
     df = df[[users_column, items_column]]
 
@@ -93,9 +106,18 @@ def convert_dataframe_to_sparse(
             inplace=False,
         )
 
-    rows = df[users_column].to_numpy(dtype=np.int32)
-    cols = df[items_column].to_numpy(dtype=np.int32)
-    data = np.ones_like(rows, dtype=np.int32)
+    rows_str = df[users_column].to_numpy(dtype="object")
+    cols_str = df[items_column].to_numpy(dtype="object")
+
+    rows = np.array([
+        mapper_user_id_to_idx[user_id]
+        for user_id in rows_str
+    ], dtype=np.int32)
+    cols = np.array([
+        mapper_item_id_to_idx[item_id]
+        for item_id in cols_str
+    ], dtype=np.int32)
+    data = np.ones_like(rows_str, dtype=np.int32)
 
     urm = sparse.COO(
         (data, (rows, cols)),
@@ -122,28 +144,15 @@ def remove_interactions_from_uim(
 
     return uim_dok.to_coo()
 
-
-def mind_small_statistics_full_dataset() -> dict:
-    config = mind_reader.MINDSmallConfig()
-
+def _compute_mind_statistics_from_config(
+    config: Union[mind_reader.MINDSmallConfig, mind_reader.MINDLargeConfig],
+) -> dict:
     pandas_raw_data = mind_reader.PandasMINDRawData(
         config=config,
     )
 
     df_raw_data = pandas_raw_data.data
     df_previous_interactions = pandas_raw_data.previous_interactions
-
-    # df = mind_reader.add_previous_interactions_to_dataframe(
-    #     df=df_raw_data,
-    #     df_previous_interactions=df_previous_interactions,
-    # )
-    #
-    # df = mind_reader.convert_user_item_impressions_dataframe(
-    #     df=df,
-    #     column_item="item_ids",
-    #     column_new_item="item_id",
-    #     column_dtype=pd.StringDtype(),
-    # )
 
     logger.debug("Computing set unique users")
     unique_users = _set_unique_users(
@@ -153,48 +162,59 @@ def mind_small_statistics_full_dataset() -> dict:
 
     logger.debug("Computing set unique items")
     unique_items = _set_unique_items(
-        df_interactions=df_raw_data,
-        df_impressions=df_impressions,
-        df_impressions_non_direct_link=df_impressions_non_direct_link,
+        df=df_raw_data,
+        df_previous_interactions=df_previous_interactions,
     )
 
-    common_sparse_shape = (max(unique_users) + 1, max(unique_items) + 1)
+    mapper_user_id_to_index = _create_mapper_id_to_indices(set_ids=unique_users)
+    mapper_item_id_to_index = _create_mapper_id_to_indices(set_ids=unique_items)
+
     num_users, num_items = len(unique_users), len(unique_items)
+    common_sparse_shape = (num_users + 1, num_items + 1)
     logger.debug(f"{num_users=} - {num_items=} - {common_sparse_shape=}")
 
-    logger.debug("Computing URM")
-    urm = convert_dataframe_to_sparse(
-        df=df_raw_data,
+    logger.debug("Computing URM previous interactions")
+    urm_previous_interactions = convert_dataframe_to_sparse(
+        df=df_previous_interactions,
         users_column="user_id",
-        items_column="series_id",
+        items_column="item_ids",
         shape=common_sparse_shape,
+        mapper_user_id_to_idx=mapper_user_id_to_index,
+        mapper_item_id_to_idx=mapper_item_id_to_index,
     )
 
+    logger.debug("Computing URM interactions")
+    urm_interactions = convert_dataframe_to_sparse(
+        df=df_raw_data,
+        users_column="user_id",
+        items_column="item_ids",
+        shape=common_sparse_shape,
+        mapper_user_id_to_idx=mapper_user_id_to_index,
+        mapper_item_id_to_idx=mapper_item_id_to_index,
+    )
+
+    logger.debug("Computing URM")
+    urm: sparse.COO = urm_previous_interactions + urm_interactions
+
     logger.debug("Computing UIM from Interactions")
-    uim_interactions = convert_dataframe_to_sparse(
+    uim_with_interactions = convert_dataframe_to_sparse(
         df=df_raw_data,
         users_column="user_id",
         items_column="impressions",
         shape=common_sparse_shape,
-    )
-
-    logger.debug("Computing UIM from Non-Direct-Link")
-    uim_non_direct_link = convert_dataframe_to_sparse(
-        df=df_impressions_non_direct_link,
-        users_column="user_id",
-        items_column="recommended_series_list",
-        shape=common_sparse_shape,
+        mapper_user_id_to_idx=mapper_user_id_to_index,
+        mapper_item_id_to_idx=mapper_item_id_to_index,
     )
 
     logger.debug("Computing UIM without Interactions")
     uim = remove_interactions_from_uim(
         urm=urm,
-        uim=uim_interactions + uim_non_direct_link,
+        uim=uim_with_interactions,
     )
 
     logger.debug("Creating statistics dictionary")
     statistics = {
-        "dataset": "ContentWise Impressions",
+        "dataset": "MIND",
         "num_users": num_users,
         "num_items": num_items,
         "num_interactions": urm.data.sum(),
@@ -206,5 +226,101 @@ def mind_small_statistics_full_dataset() -> dict:
     return statistics
 
 
-def content_wise_impressions_statistics_non_duplicates_dataset():
-    ...
+def mind_small_statistics_full_dataset() -> dict:
+    config = mind_reader.MINDSmallConfig()
+
+    statistics = _compute_mind_statistics_from_config(config=config)
+    statistics["dataset"] = "MIND Small"
+
+    return statistics
+
+    # pandas_raw_data = mind_reader.PandasMINDRawData(
+    #     config=config,
+    # )
+    #
+    # df_raw_data = pandas_raw_data.data
+    # df_previous_interactions = pandas_raw_data.previous_interactions
+    #
+    # logger.debug("Computing set unique users")
+    # unique_users = _set_unique_users(
+    #     df=df_raw_data,
+    #     df_previous_interactions=df_previous_interactions,
+    # )
+    #
+    # logger.debug("Computing set unique items")
+    # unique_items = _set_unique_items(
+    #     df=df_raw_data,
+    #     df_previous_interactions=df_previous_interactions,
+    # )
+    #
+    # mapper_user_id_to_index = _create_mapper_id_to_indices(set_ids=unique_users)
+    # mapper_item_id_to_index = _create_mapper_id_to_indices(set_ids=unique_items)
+    #
+    # num_users, num_items = len(unique_users), len(unique_items)
+    # common_sparse_shape = (num_users + 1, num_items + 1)
+    #
+    # logger.debug(f"{num_users=} - {num_items=} - {common_sparse_shape=}")
+    #
+    # logger.debug("Computing URM previous interactions")
+    # urm_previous_interactions = convert_dataframe_to_sparse(
+    #     df=df_previous_interactions,
+    #     users_column="user_id",
+    #     items_column="item_ids",
+    #     shape=common_sparse_shape,
+    #     mapper_user_id_to_idx=mapper_user_id_to_index,
+    #     mapper_item_id_to_idx=mapper_item_id_to_index,
+    # )
+    #
+    # logger.debug("Computing URM interactions")
+    # urm_interactions = convert_dataframe_to_sparse(
+    #     df=df_raw_data,
+    #     users_column="user_id",
+    #     items_column="item_ids",
+    #     shape=common_sparse_shape,
+    #     mapper_user_id_to_idx=mapper_user_id_to_index,
+    #     mapper_item_id_to_idx=mapper_item_id_to_index,
+    # )
+    #
+    # logger.debug("Computing URM")
+    # urm: sparse.COO = urm_previous_interactions + urm_interactions
+    #
+    # logger.debug("Computing UIM from Interactions")
+    # uim_with_interactions = convert_dataframe_to_sparse(
+    #     df=df_raw_data,
+    #     users_column="user_id",
+    #     items_column="impressions",
+    #     shape=common_sparse_shape,
+    #     mapper_user_id_to_idx=mapper_user_id_to_index,
+    #     mapper_item_id_to_idx=mapper_item_id_to_index,
+    # )
+    #
+    # logger.debug("Computing UIM without Interactions")
+    # uim = remove_interactions_from_uim(
+    #     urm=urm,
+    #     uim=uim_with_interactions,
+    # )
+    #
+    # logger.debug("Creating statistics dictionary")
+    # statistics = {
+    #     "dataset": "MIND Small",
+    #     "num_users": num_users,
+    #     "num_items": num_items,
+    #     "num_interactions": urm.data.sum(),
+    #     "num_unique_interactions": urm.nnz,
+    #     "num_impressions": uim.data.sum(),
+    #     "num_unique_impressions": uim.nnz,
+    # }
+    #
+    # return statistics
+
+
+def mind_large_statistics_full_dataset() -> dict:
+    config = mind_reader.MINDLargeConfig(
+        use_test_set=True,
+        use_historical_interactions=True,
+    )
+
+    statistics = _compute_mind_statistics_from_config(config=config)
+    statistics["dataset"] = "MIND"
+
+    return statistics
