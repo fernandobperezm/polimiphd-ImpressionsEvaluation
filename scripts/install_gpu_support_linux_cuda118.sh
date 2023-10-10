393482c9ec0ee1d9aa857f6e427e9bed84ebdab8
Fernando Benjamin PEREZ MAURERA <fperezmaurera@gmail.com>
2023-10-11 01:46:45 +0200

    Added a shell script that installs a CUDA 11.8 version of jax, pytorch, etc

---
+++ scripts/install_gpu_support_linux_cuda118.sh
@@ -0,0 +1,9 @@
+poetry lock --no-update
+poetry install --sync
+
+# Installs the wheel compatible with CUDA 11.8. See: https://pytorch.org/get-started/locally/
+poetry run pip install --upgrade torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 --index-url https://download.pytorch.org/whl/cu118
+
+# Installs the wheel compatible with CUDA 11.8 and cuDNN 8.6 or newer. See: https://github.com/google/jax#pip-installation-gpu-cuda-installed-locally-harder
+poetry run pip install --upgrade "jax[cuda11_local]==0.4.13" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
