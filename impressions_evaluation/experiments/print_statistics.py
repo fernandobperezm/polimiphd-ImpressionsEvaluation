695054cd5737776910015e09a7fc4510f51bb5ed
Fernando Benjamin PEREZ MAURERA <fperezmaurera@gmail.com>
2023-10-16 19:25:40 +0200

    Exporting statistics fixed and done

--- impressions_evaluation/experiments/print_statistics.py
+++ impressions_evaluation/experiments/print_statistics.py
@@ -20,12 +20,18 @@ logger = logging.getLogger(__name__)
 DIR_RESULTS_DATASETS_STATISTICS = os.path.join(
     commons.DIR_RESULTS_EXPORT,
     "datasets_statistics",
+    "",
+)
+
+DIR_RESULTS_DATASETS_STATISTICS_BENCHMARK = os.path.join(
+    DIR_RESULTS_DATASETS_STATISTICS,
     "{benchmark}",
     "{evaluation_strategy}",
     "",
 )
 
 commons.FOLDERS.add(DIR_RESULTS_DATASETS_STATISTICS)
+commons.FOLDERS.add(DIR_RESULTS_DATASETS_STATISTICS_BENCHMARK)
 
 
 ####################################################################################################
@@ -37,20 +43,64 @@ def _extended_describe(
     df: Union[pd.DataFrame, pd.Series],
     stats: list[str],
 ) -> Union[pd.DataFrame, pd.Series]:
-    if isinstance(df, pd.DataFrame):
-        d = df.describe(
-            percentiles=[0.01, 0.05, 0.1, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.75, 0.8, 0.9, 0.95, 0.99],
+    percentiles = [
+        0.01,
+        0.05,
+        0.1,
+        0.2,
+        0.25,
+        0.3,
+        0.4,
+        0.5,
+        0.6,
+        0.75,
+        0.8,
+        0.9,
+        0.95,
+        0.99,
+    ]
+
+    if isinstance(df, pd.Series):
+        df_describe = df.describe(
+            percentiles=percentiles,
         )
-        return d.append(
-            df.reindex(d.columns, axis=1).agg(stats),
+        df_stats = df.agg(stats)
+        return pd.concat(
+            objs=[df_describe, df_stats],
+            axis=0,
+            ignore_index=False,
+        )
+    elif isinstance(df, pd.DataFrame):
+        d = df.describe(
+            percentiles=[
+                0.01,
+                0.05,
+                0.1,
+                0.2,
+                0.25,
+                0.3,
+                0.4,
+                0.5,
+                0.6,
+                0.75,
+                0.8,
+                0.9,
+                0.95,
+                0.99,
+            ],
+        )
+        import pdb
+
+        pdb.set_trace()
+
+        df_stats = df.reindex(d.columns, axis=1).agg(stats)
+        return pd.concat(
+            objs=[d, df_stats],
+            axis=0,
+            ignore_index=False,
         )
     else:
-        d = df.describe(
-            percentiles=[0.01, 0.05, 0.1, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.75, 0.8, 0.9, 0.95, 0.99],
-        )
-        return d.append(
-            df.reindex().agg(stats),
-        )
+        return pd.DataFrame(data=[])
 
 
 def _compute_statistics_csr_matrix(
@@ -60,16 +110,40 @@ def _compute_statistics_csr_matrix(
 ) -> dict[str, Any]:
     from Data_manager.Dataset import gini_index
 
+    sp_matrix_coo = sp_matrix.tocoo()
+
     results_dict = {
         "dataset": benchmark.value,
         "matrix_name": sp_matrix_name,
         "nnz": sp_matrix.nnz,
         "num_rows": sp_matrix.shape[0],
         "num_col": sp_matrix.shape[1],
+        "num_unique_users": np.unique(
+            sp_matrix_coo.row,
+            return_index=False,
+            return_counts=False,
+            return_inverse=False,
+        ).size,
+        "num_unique_items": np.unique(
+            sp_matrix_coo.col,
+            return_index=False,
+            return_counts=False,
+            return_inverse=False,
+        ).size,
+    }
+
+    results_dict = {
+        **results_dict,
+        "density": results_dict["nnz"]
+        / (results_dict["num_rows"] * results_dict["num_col"]),
+        "users_per_row": results_dict["num_unique_users"] / results_dict["num_rows"],
+        "items_per_row": results_dict["num_unique_items"] / results_dict["num_col"],
     }
 
     items = [
         ("data", sp_matrix.data),
+        ("users", sp_matrix_coo.row),
+        ("items", sp_matrix_coo.col),
         ("user_profile_length", np.ediff1d(sp_matrix.indptr)),
         ("item_profile_length", np.ediff1d(sp_matrix.tocsc().indptr)),
     ]
@@ -77,12 +151,9 @@ def _compute_statistics_csr_matrix(
     for name, data in items:
         data_stats_describe = _extended_describe(
             df=pd.Series(data=data),
-            stats=["var", 'skew', 'mad', 'kurt', 'sum']
+            stats=["var", "mean", "std", "median", "skew", "kurt", "sum", "count"],
         ).to_dict()
-        results_dict.update({
-            f"{name}_{k}": v
-            for k,v in data_stats_describe.items()
-        })
+        results_dict.update({f"{name}_{k}": v for k, v in data_stats_describe.items()})
         results_dict[f"{name}_gini_index"] = gini_index(data)
 
     return results_dict
@@ -92,7 +163,7 @@ def _print_dataset_experiments_statistics(
     experiment_benchmark: commons.ExperimentBenchmark,
     experiment_hyper_parameters: commons.HyperParameterTuningParameters,
     folder_path: str,
-) -> None:
+) -> pd.DataFrame:
     benchmark_reader = commons.get_reader_from_benchmark(
         benchmark_config=experiment_benchmark.config,
         benchmark=experiment_benchmark.benchmark,
@@ -205,49 +276,90 @@ def _print_dataset_experiments_statistics(
         )
 
     cases = [
-        dict(sp_matrix=interactions_data_splits.sp_urm_train, sp_matrix_name="urm_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=interactions_data_splits.sp_urm_validation, sp_matrix_name="urm_validation",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=interactions_data_splits.sp_urm_train_validation, sp_matrix_name="urm_train_validation",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=interactions_data_splits.sp_urm_test, sp_matrix_name="urm_test",
-             benchmark=experiment_benchmark.benchmark),
-
-        dict(sp_matrix=impressions_data_splits.sp_uim_train, sp_matrix_name="uim_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_data_splits.sp_uim_validation, sp_matrix_name="uim_validation",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_data_splits.sp_uim_train_validation, sp_matrix_name="uim_train_validation",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_data_splits.sp_uim_test, sp_matrix_name="uim_test",
-             benchmark=experiment_benchmark.benchmark),
-
-        dict(sp_matrix=impressions_feature_frequency_train, sp_matrix_name="uim_frequency_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_feature_frequency_train_validation, sp_matrix_name="uim_frequency_train_validation",
-             benchmark=experiment_benchmark.benchmark),
-
-        dict(sp_matrix=impressions_feature_position_train, sp_matrix_name="uim_position_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_feature_position_train_validation, sp_matrix_name="uim_position_train_validation",
-             benchmark=experiment_benchmark.benchmark),
-
-        dict(sp_matrix=impressions_feature_timestamp_train, sp_matrix_name="uim_timestamp_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_feature_timestamp_train_validation, sp_matrix_name="uim_timestamp_train_validation",
-             benchmark=experiment_benchmark.benchmark),
-
-        dict(sp_matrix=impressions_feature_last_seen_train, sp_matrix_name="uim_last_seen_train",
-             benchmark=experiment_benchmark.benchmark),
-        dict(sp_matrix=impressions_feature_last_seen_train_validation, sp_matrix_name="uim_last_seen_train_validation",
-             benchmark=experiment_benchmark.benchmark),
+        dict(
+            sp_matrix=interactions_data_splits.sp_urm_train,
+            sp_matrix_name="urm_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=interactions_data_splits.sp_urm_validation,
+            sp_matrix_name="urm_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=interactions_data_splits.sp_urm_train_validation,
+            sp_matrix_name="urm_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=interactions_data_splits.sp_urm_test,
+            sp_matrix_name="urm_test",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_data_splits.sp_uim_train,
+            sp_matrix_name="uim_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_data_splits.sp_uim_validation,
+            sp_matrix_name="uim_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_data_splits.sp_uim_train_validation,
+            sp_matrix_name="uim_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_data_splits.sp_uim_test,
+            sp_matrix_name="uim_test",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_frequency_train,
+            sp_matrix_name="uim_frequency_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_frequency_train_validation,
+            sp_matrix_name="uim_frequency_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_position_train,
+            sp_matrix_name="uim_position_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_position_train_validation,
+            sp_matrix_name="uim_position_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_timestamp_train,
+            sp_matrix_name="uim_timestamp_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_timestamp_train_validation,
+            sp_matrix_name="uim_timestamp_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_last_seen_train,
+            sp_matrix_name="uim_last_seen_train",
+            benchmark=experiment_benchmark.benchmark,
+        ),
+        dict(
+            sp_matrix=impressions_feature_last_seen_train_validation,
+            sp_matrix_name="uim_last_seen_train_validation",
+            benchmark=experiment_benchmark.benchmark,
+        ),
     ]
     results = []
     for case in cases:
-        results.append(
-            _compute_statistics_csr_matrix(**case)
-        )
+        results.append(_compute_statistics_csr_matrix(**case))
 
     df_results = pd.DataFrame.from_records(
         data=results,
@@ -260,6 +372,9 @@ def _print_dataset_experiments_statistics(
             header=True,
             encoding="utf-8",
             na_rep="-",
+            sep=";",
+            compression=None,
+            decimal=",",
         )
         df_results.to_latex(
             buf=os.path.join(folder_path, "dataset_statistics.tex"),
@@ -272,41 +387,79 @@ def _print_dataset_experiments_statistics(
             longtable=True,
         )
 
+    return df_results
+
 
 def print_datasets_statistics(
     experiment_cases_interface: commons.ExperimentCasesInterface,
 ) -> None:
-    printed_experiments: set[tuple[commons.Benchmarks, commons.EHyperParameterTuningParameters]] = set()
+    printed_experiments: set[
+        tuple[commons.Benchmarks, commons.EHyperParameterTuningParameters]
+    ] = set()
 
     baseline_benchmarks = experiment_cases_interface.to_use_benchmarks
-    baseline_hyper_parameters = experiment_cases_interface.to_use_hyper_parameter_tuning_parameters
+    baseline_hyper_parameters = (
+        experiment_cases_interface.to_use_hyper_parameter_tuning_parameters
+    )
 
-    for benchmark, hyper_parameters in itertools.product(baseline_benchmarks, baseline_hyper_parameters):
+    results = []
+
+    for benchmark, hyper_parameters in itertools.product(
+        baseline_benchmarks, baseline_hyper_parameters
+    ):
         if (benchmark, hyper_parameters) in printed_experiments:
             continue
         else:
             printed_experiments.add((benchmark, hyper_parameters))
 
-        experiment_benchmark = commons.MAPPER_AVAILABLE_BENCHMARKS[
-            benchmark
-        ]
-        experiment_hyper_parameters = commons.MAPPER_AVAILABLE_HYPER_PARAMETER_TUNING_PARAMETERS[
-            hyper_parameters
-        ]
+        experiment_benchmark = commons.MAPPER_AVAILABLE_BENCHMARKS[benchmark]
+        experiment_hyper_parameters = (
+            commons.MAPPER_AVAILABLE_HYPER_PARAMETER_TUNING_PARAMETERS[hyper_parameters]
+        )
 
-        folder_path_export = DIR_RESULTS_DATASETS_STATISTICS.format(
+        folder_path_export = DIR_RESULTS_DATASETS_STATISTICS_BENCHMARK.format(
             benchmark=experiment_benchmark.benchmark.value,
             evaluation_strategy=experiment_hyper_parameters.evaluation_strategy.value,
         )
 
         os.makedirs(folder_path_export, exist_ok=True)
 
-        _print_dataset_experiments_statistics(
+        df_results = _print_dataset_experiments_statistics(
             experiment_benchmark=experiment_benchmark,
             experiment_hyper_parameters=experiment_hyper_parameters,
             folder_path=folder_path_export,
         )
+        results.append(df_results)
 
-        logger.info(
-            f"Successfully finished exporting accuracy and beyond-accuracy results to LaTeX"
+    df_results = pd.concat(
+        objs=results,
+        axis=0,
+        ignore_index=True,
+    )
+
+    folder_path_export = DIR_RESULTS_DATASETS_STATISTICS
+    os.makedirs(folder_path_export, exist_ok=True)
+
+    with pd.option_context("max_colwidth", 1000):
+        df_results.to_csv(
+            path_or_buf=os.path.join(folder_path_export, "datasets_statistics.csv"),
+            index=True,
+            header=True,
+            encoding="utf-8",
+            na_rep="-",
+            sep=";",
+            compression=None,
+            decimal=",",
         )
+        df_results.to_latex(
+            buf=os.path.join(folder_path_export, "datasets_statistics.tex"),
+            index=True,
+            header=True,
+            escape=False,
+            float_format="{:.4f}".format,
+            encoding="utf-8",
+            na_rep="-",
+            longtable=True,
+        )
+
+    logger.info(f"Successfully finished exporting statistics of datasets.")
